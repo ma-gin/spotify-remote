e3ef812144ffd9ce96138a0c4612a0958468ecb6
Matt Ginter <mattginter@gmail.com>
2022-02-18 13:02:34 +0100

    initialize components

---
+++ src/components/Display.js
@@ -0,0 +1,5 @@
+export default function Display () {
+    return (
+        <h1>Hello Display</h1>
+    )
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,5 @@
+export default function Sidebar () {
+    return (
+        <h1>Hello Sidebar</h1>
+    )
+}
